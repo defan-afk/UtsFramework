3928907c6d4fdd2cc782678e01aab94f87f8b6f5
Getar jagat Dewantara <mcoc897@gmail.com>
2021-04-20 22:15:10 +0700

    first commit

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -10,7 +10,7 @@
         <div class="container">
             <div class="card mt-5">
                 <div class="card-header bg-primary text-center">
-                    CRUD Data Pegawai - <a href="https://kagion.github.io/" target="_blank"> Getar jagat Dewantara</a>
+                    CRUD Data Pegawai - <a href="#" target="_blank"> Getar jagat Dewantara</a>
                 </div>
                 <div class="card-body">
                     <a href="/pegawai/tambah" class="btn btn-primary">Input Pegawai</a>
