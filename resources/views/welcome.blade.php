3c224ded8088f4aa05e56afc6b6c6310b2b7d263
Getar jagat Dewantara <mcoc897@gmail.com>
2021-04-21 17:18:36 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,6 +38,7 @@
             <div class="home-content">
                 <div class="text-1">Name </div>
                 <div class="text-2">Getar Jagat Dewantara</div>
+                <div class="text-3"><span class="typing"></span></div>
                 <a href="#">Start</a>
             </div>
           </div>
