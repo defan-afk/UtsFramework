ae82cec82311ac1cb55ae4f2a84bb09206e0385f
Getar jagat Dewantara <mcoc897@gmail.com>
2021-04-21 17:34:07 +0700

    first commit

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
         <title>UTS 6B D4 Teknik Informatika</title>
     </head>
