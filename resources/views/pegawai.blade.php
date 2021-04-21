eaa9e4cffec586db18dc261bb101e5092d7f7434
Getar jagat Dewantara <mcoc897@gmail.com>
2021-04-21 17:43:25 +0700

    first commit

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -10,11 +10,12 @@
     <body>
         <div class="container">
             <div class="card mt-5">
+            <div class="max-width">
                 <div class="card-header bg-danger text-center">
                     CRUD Data Pegawai - <a href="#" target="_blank"> Getar Jagat Dewantara</a>
                 </div>
                 <div class="card-body">
-                <div class="max-width">
+                
                     <a href="/pegawai/tambah" class="btn btn-dark">Input Pegawai</a>
                     <br/>
                     <br/>
